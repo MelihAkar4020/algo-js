88d9fcaeb5d485a27af6c2543a404e39f36e8b9a
MelihAkar4020 <mlhkr55@gmail.com>
2022-05-24 13:31:39 +0200

    ex 1

--- exercice1/1.6.js
+++ exercice1/1.6.js
@@ -1,2 +1,8 @@
 const readlineSync = require("readline-sync");
 
+var num1 = new Number(readlineSync.question("QUel est le premier nombre ? "));
+var num2 = new Number(readlineSync.question("Quel est le seconde nombre ? "));
+
+console.log('Le quotient des deux nombres est de ' + (num1/num2) ) ;
+
+console.log('Le reste des deux nombres est de ' + (num1%num2) ) ;
